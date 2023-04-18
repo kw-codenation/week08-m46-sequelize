a097f48f53dad58f70b353493d07a96fe6008b4c
kw-codenation <kw-lea-codenation@gmx.co.uk>
2023-04-18 14:04:53 +0100

    add server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/health', (req, res) => 
+    {
+        res.status(200).json({ message: 'App is healthy' })
+    }
+)
+
+app.listen(5001, () => 
+    {
+        console.log('Server is listening')
+    }
+)
