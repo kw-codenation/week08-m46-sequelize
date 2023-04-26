8148d730a4db44e50b9bf86c311db0c8e57c9fd4
kw-codenation <kw-lea-codenation@gmx.co.uk>
2023-04-26 16:45:42 +0100

    final comments

--- src/authors/controllers.js
+++ src/authors/controllers.js
@@ -1,166 +1,175 @@
 const Author = require('./model')
 
+// add an author
 const addAuthor = async (req, res) =>     
+{
+    try 
     {
-        try 
-        {
-            console.log(req.body)
-            const author = await Author.create
-            (
-                {name: req.body.name
-                ,bname: req.body.bname
-                ,bdate: req.body.bdate
-                ,bplace: req.body.bplace
+        console.log(req.body)
+        const author = await Author.create
+        (
+            {name: req.body.name
+            ,bname: req.body.bname
+            ,bdate: req.body.bdate
+            ,bplace: req.body.bplace
+            }
+        )
+
+        res.status(201).json({message: author.name +' has been added successfully id: ' + author.id, authors:author})
+    } 
+    catch (error) 
+    {
+        console.log('addAuthor error: ' + error)    
+    }
+}
+
+
+// get an author
+const getAllAuthors = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        const authors = await Author.findAll()
+
+        res.status(201).json({message: 'success (' + authors.length + ') found', authors:authors})
+    } 
+    catch (error) 
+    {
+        console.log('addAllAuthors error ' + error)  
+    }
+
+}
+
+// find an author
+const findAuthors = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        const authors = await Author.findAll({where:req.body})
+
+        res.status(201).json({message: 'success ' + authors.length + ' found', authors:authors})
+    } 
+    catch (error) 
+    {
+        console.log(error)    
+    }
+
+}
+
+// delete an author
+const deleteAuthor = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body.id)
+        const rows = await Author.destroy({where:req.body})
+
+        res.status(203).json({message: 'author with id ' + JSON.stringify(req.body.id) + ' successfully deleted', rows:rows})
+    } 
+    catch (error) 
+    {
+        console.log('deleteAuthor error: ' + error)    
+    }
+
+}
+
+
+// update an author
+const updateAuthor = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        const rows = await Author.update
+                            (req.body.criteria,
+                            {where:req.body.where}
+                            )
+
+        res.status(200).json({message: 'Update of author id (' + req.body.where.id + ') using criteria ' + JSON.stringify(req.body.criteria) + ' successful', rows:rows})
+    } 
+    catch (error) 
+    {
+        console.log('updateAuthor: ' + error)    
+    }
+
+}
+
+// load authors onto the table 'author'
+const loadAuthors = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        const author = await Author.bulkCreate
+        (
+            [
+                {
+                    "name":"Agatha Christie",
+                    "bname":"Agatha Mary Clarissa Miller",
+                    "bdate":"15th September 1890",
+                    "bplace":"Torquay, Devon, England"
+                },
+                {
+                    "name":"Jules Verne",
+                    "bname":"Jules Gabriel Verne",
+                    "bdate":"8th February 1828",
+                    "bplace":"Nantes, Brittany, France"
+                },
+                {
+                    "name":"Dorothy L. Sayers",
+                    "bname":"Dorothy Leigh Sayers",
+                    "bdate":"13th June 1893",
+                    "bplace":"Oxford, England"
+                },
+                {
+                    "name":"Alexandre Dumas",
+                    "bname":"Dumas Davy de la Pailleterie",
+                    "bdate":"24th July 1802",
+                    "bplace":"Villers-Cotterêts, Picardy, France"
+                },
+                {
+                    "name":"Charlotte Brontë",
+                    "bname":"Charlotte Brontë",
+                    "bdate":"21st April 1816",
+                    "bplace":"Haworth, Yorkshire, England"
+                },
+                {
+                    "name":"Arthur Conan Doyle",
+                    "bname":"Arthur Ignatius Conan Doyle",
+                    "bdate":"22nd May 1859",
+                    "bplace":"Crowborough, Sussex, England"
+                },
+                {
+                    "name":"HG Wells",
+                    "bname":"Herbert George Wells",
+                    "bdate":"21st September 1866",
+                    "bplace":"Bromley, Kent, England"
+                },
+                {
+                    "name":"Jane Austen",
+                    "bname":"Jane Austen",
+                    "bdate":"16th December 1775",
+                    "bplace":"Steventon, Hampshire, England"
+                },
+                {
+                    "name":"Daphne du Maurier",
+                    "bname":"Daphne du Maurier",
+                    "bdate":"13th May 1907",
+                    "bplace":"London, England"
                 }
-            )
+            ]
+        )
 
-            res.status(201).json({message: author.name +' has been added successfully id: ' + author.id, authors:author})
-        } 
-        catch (error) 
-        {
-            console.log('addAuthor error: ' + error)    
-        }
-    }
-
-    const getAllAuthors = async (req, res) =>     
+        res.status(201).json({message: '9 authors has been added successfully id: ' + author.id, author:author})
+    } 
+    catch (error) 
     {
-        try 
-        {
-            console.log(req.body)
-            const authors = await Author.findAll()
-
-            res.status(201).json({message: 'success (' + authors.length + ') found', authors:authors})
-        } 
-        catch (error) 
-        {
-            console.log('addAllAuthors error ' + error)  
-        }
-
+        console.log(error)    
     }
 
-    const findAuthors = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            const authors = await Author.findAll({where:req.body})
-
-            res.status(201).json({message: 'success ' + authors.length + ' found', authors:authors})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
-
-    const deleteAuthor = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body.id)
-            await Author.destroy({where:req.body})
-
-            res.status(203).json({message: 'author with id ' + JSON.stringify(req.body.id) + ' successfully deleted'})
-        } 
-        catch (error) 
-        {
-            console.log('deleteAuthor error: ' + error)    
-        }
-
-    }
-
-    const updateAuthor = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            const author = await Author.update
-                                (req.body.criteria,
-                                {where:req.body.where}
-                                )
-
-            res.status(200).json({message: 'Update of author id (' + req.body.where.id + ') using criteria ' + JSON.stringify(req.body.criteria) + ' successful', author:author})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
-    const loadAuthors = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            const author = await Author.bulkCreate
-            (
-                [
-                    {
-                      "name":"Agatha Christie",
-                      "bname":"Agatha Mary Clarissa Miller",
-                      "bdate":"15th September 1890",
-                      "bplace":"Torquay, Devon, England"
-                    },
-                    {
-                      "name":"Jules Verne",
-                      "bname":"Jules Gabriel Verne",
-                      "bdate":"8th February 1828",
-                      "bplace":"Nantes, Brittany, France"
-                    },
-                    {
-                      "name":"Dorothy L. Sayers",
-                      "bname":"Dorothy Leigh Sayers",
-                      "bdate":"13th June 1893",
-                      "bplace":"Oxford, England"
-                    },
-                    {
-                      "name":"Alexandre Dumas",
-                      "bname":"Dumas Davy de la Pailleterie",
-                      "bdate":"24th July 1802",
-                      "bplace":"Villers-Cotterêts, Picardy, France"
-                    },
-                    {
-                        "name":"Charlotte Brontë",
-                        "bname":"Charlotte Brontë",
-                        "bdate":"21st April 1816",
-                        "bplace":"Haworth, Yorkshire, England"
-                    },
-                    {
-                        "name":"Arthur Conan Doyle",
-                        "bname":"Arthur Ignatius Conan Doyle",
-                        "bdate":"22nd May 1859",
-                        "bplace":"Crowborough, Sussex, England"
-                    },
-                    {
-                        "name":"HG Wells",
-                        "bname":"Herbert George Wells",
-                        "bdate":"21st September 1866",
-                        "bplace":"Bromley, Kent, England"
-                    },
-                    {
-                        "name":"Jane Austen",
-                        "bname":"Jane Austen",
-                        "bdate":"16th December 1775",
-                        "bplace":"Steventon, Hampshire, England"
-                    },
-                    {
-                        "name":"Daphne du Maurier",
-                        "bname":"Daphne du Maurier",
-                        "bdate":"13th May 1907",
-                        "bplace":"London, England"
-                    }
-                ]
-            )
-
-            res.status(201).json({message: '9 authors has been added successfully id: ' + author.id, author:author})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
+}
 
 
 module.exports = {
--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,187 +1,209 @@
 const Book = require('./model')
-const Author = require('../authors/model')
-const Genre = require('../genres/model')
 
+// add a book - requires a valid author and genre id
 const addBook = async (req, res) =>     
+{
+    try 
     {
-        try 
+        console.log(req.body)
+        const book = await Book.create
+        (
+            {title: req.body.title
+            ,authorId: req.body.authorId
+            ,genreId: req.body.genreId
+            }
+        )
+
+        res.status(201).json({message: book.title +' has been added successfully id: ' + book.id, books:book})
+    } 
+    catch (error) 
+    {
+        console.log('addBook error:' + error)    
+    }
+
+}
+
+//get all the books in the table books, this will include
+// all assoicated author and genre data
+const getAllBooks = async (req, res) =>     
+{
+    try 
+    {            
+        console.log(req.body)
+        const books = await Book.findAll({include: [{all:true}]})
+        res.status(201).json({message: 'success (' + books.length + ') found', books:books, count:books.length})
+    } 
+    catch (error) 
+    {
+        console.log('getAllBooks error: ' + error)    
+    }
+
+}
+
+// This is supposed to be the one link for all finds
+// and the criteria is determined by the key 'search'
+//
+// search = 'author' will search for books by a particular author
+//
+// search = 'genre' will search for all books with a particular genre
+//
+// search = '' will find all books
+//
+//
+const findBooks = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        
+        let books = ''
+        
+        if (req.body.search == 'author')
         {
-            console.log(req.body)
-            const book = await Book.create
-            (
-                {title: req.body.title
-                ,authorId: req.body.authorId
-                ,genreId: req.body.genreId
+            books = await Book.findAll({where: {authorId: req.body.value }, include: [{all:true}]})
+        }
+        else if (req.body.search == 'genre')
+        {
+            books = await Book.findAll({where: {genreId: req.body.value }, include: [{all:true}]})
+        }
+        else
+        {
+            books = await Book.findAll({include: [{all:true}]})
+        }
+
+        res.status(201).json({message: 'success ' + books.length + ' found', count:books.length, books:books})
+        
+    } 
+    catch (error) 
+    {
+        console.log('findBook error: ' + error)    
+    }
+}
+
+// delete a book using an Id
+const deleteBook = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        await Book.destroy({where:req.body})
+
+        res.status(201).json({message: 'book using criteria ' + JSON.stringify(req.body) + ' successfully deleted'})
+    } 
+    catch (error) 
+    {
+        console.log(error)    
+    }
+
+}
+
+// delete all books on the book table
+const deleteAllBooks = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        await Book.destroy()
+
+        res.status(201).json({message: 'all rows on the book table successfully deleted'})
+    } 
+    catch (error) 
+    {
+        console.log(error)    
+    }
+
+}
+
+// update book, two keys needed 'criteria' and 'where'
+//
+// criteria = {"title":"A Great Book"}
+// where = {"author":"A Fantastic Author"}
+//
+// this will change the title of a book to "A Great Book"
+// for every book in the table 'books' that arewritten 
+// by 'A Fansatcid Author'
+//
+const updateBook = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        const book = await Book.update
+                            (req.body.criteria,
+                            {where:req.body.where}
+                            )
+
+        res.status(200).json({message: 'Update of book id (' + req.body.where.id + ') using criteria ' + JSON.stringify(req.body.criteria) + ' successful', book:book})
+    } 
+    catch (error) 
+    {
+        console.log(error)    
+    }
+
+}
+
+ // load 8 books on to the table 'books'
+ // there are equlivalent links for 'authors'
+ // and 'genres' and must be run first before
+ // executing loadBooks   
+const loadBooks = async (req, res) =>     
+{
+    try 
+    {
+        console.log(req.body)
+        await Book.bulkCreate
+        (
+            [
+                {
+                    "title":"The Three Musketeers",
+                    "authorId":4,
+                    "genreId":3
+                },
+                {
+                    "title":"The Nine Tailers",
+                    "authorId":3,
+                    "genreId":8
+                },
+                {
+                    "title":"Around The World In Eighty Days",
+                    "authorId":2,
+                    "genreId":3
+                },
+                {
+                    "title":"Death On The Nile",
+                    "authorId":1,
+                    "genreId":8
+                },
+                {
+                    "title":"Twenty Thousand Leagues Under the Sea",
+                    "authorId":2,
+                    "genreId":6
+                },
+                {
+                    "title":"Evil Under The Sun",
+                    "authorId":1,
+                    "genreId":8
+                },
+                {
+                    "title":"The Man In The Iron Mask",
+                    "authorId":4,
+                    "genreId":3
+                },
+                {
+                    "title":"Gaudy Night",
+                    "authorId":3,
+                    "genreId":8
                 }
-            )
+            ]
+        )
 
-            res.status(201).json({message: book.title +' has been added successfully id: ' + book.id, books:book})
-        } 
-        catch (error) 
-        {
-            console.log('addBook error:' + error)    
-        }
-
-    }
-
-    const getAllBooks = async (req, res) =>     
+        res.status(201).json({message: '8 books have been loaded on to the the book table successfully'})
+    } 
+    catch (error) 
     {
-        try 
-        {
-            console.log(req.body)
-            const books = await Book.findAll({include: [{all:true}]})
-
-            res.status(201).json({message: 'success (' + books.length + ') found', books:books, count:books.length})
-        } 
-        catch (error) 
-        {
-            console.log('getAllBooks error: ' + error)    
-        }
-
+        console.log(error)    
     }
-
-    const findBooks = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            
-            let books = ''
-            if (req.body.search === 'none')
-            {
-                console.log('none')
-                books = await Book.findAll({include: [{all:true}]})
-            }
-            else if (req.body.search == 'author')
-            {
-                books = await Book.findAll({where: {authorId: req.body.value }, include: [{all:true}]})
-            }
-            else if (req.body.search == 'genre')
-            {
-                books = await Book.findAll({where: {genreId: req.body.value }, include: [{all:true}]})
-            }
-
-            res.status(201).json({message: 'success ' + books.length + ' found', count:books.length, books:books})
-            
-        } 
-        catch (error) 
-        {
-            console.log('findBook error: ' + error)    
-        }
-
-    }
-
-    const deleteBook = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            await Book.destroy({where:req.body})
-
-            res.status(201).json({message: 'book using criteria ' + JSON.stringify(req.body) + ' successfully deleted'})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
-
-    const deleteAllBooks = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            await Book.destroy()
-
-            res.status(201).json({message: 'all rows on the book table successfully deleted'})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
-
-    const updateBook = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            const book = await Book.update
-                                (req.body.criteria,
-                                {where:req.body.where}
-                                )
-
-            res.status(200).json({message: 'Update of book id (' + req.body.where.id + ') using criteria ' + JSON.stringify(req.body.criteria) + ' successful', book:book})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
-
-    
-    const loadBooks = async (req, res) =>     
-    {
-        try 
-        {
-            console.log(req.body)
-            await Book.bulkCreate
-            (
-                [
-                    {
-                        "title":"The Three Musketeers",
-                        "authorId":4,
-                        "genreId":3
-                    },
-                    {
-                        "title":"The Nine Tailers",
-                        "authorId":3,
-                        "genreId":8
-                    },
-                    {
-                        "title":"Around The World In Eighty Days",
-                        "authorId":2,
-                        "genreId":3
-                    },
-                    {
-                        "title":"Death On The Nile",
-                        "authorId":1,
-                        "genreId":8
-                    },
-                    {
-                        "title":"Twenty Thousand Leagues Under the Sea",
-                        "authorId":2,
-                        "genreId":6
-                    },
-                    {
-                        "title":"Evil Under The Sun",
-                        "authorId":1,
-                        "genreId":8
-                    },
-                    {
-                        "title":"The Man In The Iron Mask",
-                        "authorId":4,
-                        "genreId":3
-                    },
-                    {
-                        "title":"Gaudy Night",
-                        "authorId":3,
-                        "genreId":8
-                    }
-                ]
-            )
-
-            res.status(201).json({message: '8 books have been loaded on to the the book table successfully'})
-        } 
-        catch (error) 
-        {
-            console.log(error)    
-        }
-
-    }
+}
 
 
 module.exports = {
